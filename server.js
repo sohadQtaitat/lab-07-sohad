bed1bc25cf712ccc53b5f37c5a5a2b3d79313aed
sohadQtaitat <qtaitat3@gmail.com>
2019-11-29 12:10:13 +0200

    lab 7

--- server.js
+++ server.js
@@ -14,106 +14,98 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 
+
+
 //////////////////////////////////////////////////------------ Routes ------------/////////////////////////////Sohad/
 
 //////////////////////////////////////////////////------------ API routes------------/////////////////////////////Sohad/
 
 
-
-app.get('/location', locationFind);
-
-app.get('/weather', weatherFind);
-
-app.get('/events', eventData);
+app.get('/location',locationHandler);
+app.get('/weather', weatherHanddler);
+app.get('/events', eventHanddler);
 
 
+//////////////////Location////////////////////
 
+  function locationHandler(req,res){
+getlocation(req.query.data)
+.then(locationData => res.status(200).json(locationData));
+  };
 
+  
+    function getlocation (city){
+        const url =`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${process.env.GEOCODE_API_KEY}`
+        return superagent.get(url)
+        .then( data =>{
+            return new Location (city , data.body)
+        })
+    };
 
-
-// function to get location data
-function locationFind(request, response) {
-
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
-
-
-  superagent.get(url)
-    .then(result => {
-      const location = new Location(result, request.query.data);
-      response.send(location);
-    })
-    .catch(err => handleError(err, response));
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-function weatherFind(request, response) {
 
     
-  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
-
-
-  superagent.get(url)
-    .then(result => {
-      const weatherStatues = result.body.daily.data.map(day => new Weather(day));
-    //   console.log('soo', weatherStatues);
-      response.send(weatherStatues);
-    })
-    .catch(err => handleError(err, response));
-}
+    function Location( city ,data ) {
+        this.search_query = city;
+        this.formatted_query = data.results[0].formatted_address;
+        this.latitude = data.results[0].geometry.location.lat;
+      this.longitude = data.results[0].geometry.location.lng;
+  }
 
 
 
+/////////////////Weather///////////////////////
+  function weatherHanddler(req,res) {
+    getWeather(req.query.data)
+       .then (weatherData => res.status(200).json(weatherData) );
+   };
+   
+   
+   
+   
+   function getWeather (query) {
+     const url = `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${query.latitude},${query.longitude}`;
+     return superagent.get(url)
+     .then( data => {
+       let weather = data.body;
+       return weather.daily.data.map( (day) => {
+         return new Weather(day);
+        });
+      });
+    };
+    function Weather( day ) {
+      
+      this.forecast = day.summary;
+      this.time = new Date(day.time * 1000).toDateString();
+      
+    }
 
 
+////////////Event/////////////////////////
+    function eventHanddler(req,res) {
+     getEventINFO(req.query.data)
+        .then (eventData => res.status(200).json(eventData) );
+    };
+    function getEventINFO (query) {
+    const url = `http://api.eventful.com/json/events/search?app_key=${process.env.EVENTS_API}&location=${query.formatted_query}`;
+    return superagent.get(url)
+    .then( data => {
+      let eventl = JSON.parse(data.text);
+      return eventl.events.event.map( (day) => {
+        return new Event(day);
+      });
+    });
+  };
 
-function eventData(request, response) {
+  
 
-  const url = `https://www.eventbriteapi.com/v3/events/search/token=${process.env.EVENTBRITE_API_KEY}&location.address=${request.query.data.formatted_query}`;
+ function Event (day){
+              this.link = day.url ;
+              this.name = day.title ;
+              this.event_date = day.start_time ;
+              this.summary = day.description 
 
-  superagent.get(url)
-    .then(result => {
-      const eventStatues = result.body.events.map(event => new Event(event));
-      response.send(eventStatues);
-    })
-    .catch(error => handleError(error, response));
-}
-
-
-
-
-
-function Location(data, userData) {
-  this.formatted_query = data.body.results[0].formatted_address;
-  this.latitude = data.body.results[0].geometry.location.lat;
-  this.longitude = data.body.results[0].geometry.location.lng;
-  this.query = userData;
-}
-
-
-function Weather(day) {
-  let time = new Date(day.time * 1000);
-  this.time = time.toDateString();
-  this.forecast = day.summary;
-}
-
-function Event(event) {
-  this.link = event.url;
-  this.name = event.name.text;
-  this.event_date = new Date(event.start.local).toString().slice(0, 15);
-  this.summary = event.summary;
-}
-
+          }
+         
 
 
 /////////////////////////////////////////////////-----------Error------------/////////////////////////////Sohad/
@@ -127,7 +119,13 @@ app.get('/foo',(request,response) =>{
 
 app.use('*', (request, response) => {
     response.status(404).send('Not Found')
-})
+  })
+
+  
+app.get('/', (request,response)=>{
+  response.status(200).send("Hi :)");
+});
+
 
 app.use((error,request,response) => {
     response.status(500).send(error)
@@ -137,3 +135,4 @@ app.use((error,request,response) => {
 
  
 app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
+
